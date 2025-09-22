69dae5c565474876b20aa05763b0833c228aaff0
Priyanshi-2k6 <202301226@dau.ac.in>
2025-09-22 21:50:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Signup from './Signup.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Homepage from '../src/Components/HomePage/Homepage.jsx';
+import Signup from '../src/Components/Signup_And_Login/Signup.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Signup />} />
+    </>
+  )
+);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Signup />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
