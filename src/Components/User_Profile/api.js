3d12f890afe62460c4643d58a5b2cf5214388949
da-harshilv14 <202301208@dau.ac.in>
2025-10-28 00:00:53 +0530

    updated forgotPassword.jsx and api.js for now

--- src/Components/User_Profile/api.js
+++ src/Components/User_Profile/api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://127.0.0.1:8000/profile",
-    baseURL: "https://krushisetu-backend-production-4a02.up.railway.app/api",
+    baseURL: "http://127.0.0.1:8000/profile",
+    // baseURL: "https://krushisetu-backend-production-4a02.up.railway.app/api",
     withCredentials: true,
 });
 
