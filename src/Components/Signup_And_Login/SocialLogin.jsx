ef5fcfd3a75b96bf5f6e27473a438bf832524b89
da-harshilv14 <202301208@dau.ac.in>
2025-10-28 00:19:22 +0530

    Update SocialLogin.jsx

--- src/Components/Signup_And_Login/SocialLogin.jsx
+++ src/Components/Signup_And_Login/SocialLogin.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from 'react-router-dom';
-import api from './api'; 
+import api from './api';
+import { Toaster, toast } from 'react-hot-toast';
 
 // Social Login Component
 function SocialLogin() {
@@ -17,40 +18,44 @@ function SocialLogin() {
                 token,
             });
 
-            console.log("Backend response:", res.data);
-            alert("Login successful! Check console for details.");
+            toast.success("Logged in successfully!");
 
             // Save JWT tokens (for later authenticated requests)
             localStorage.setItem("access", res.data.access);
             localStorage.setItem("refresh", res.data.refresh);
             // redirect to sidebar after successful login
-            navigate('/sidebar');
+            setTimeout(() => {
+                navigate('/sidebar');
+            }, 3000);
         } catch (err) {
             console.error("Login failed:", err.response?.data || err.message);
-            alert("Login failed. Check console.");
+            toast.error("Login failed!");
         }
     };
 
     return (
-        <div>
-            <div className="flex items-center">
-                <hr className="border-t-2 w-15 ml-4" />
-                <p className="ml-4 mr-2">Or continue with</p>
-                <hr className="border-t-2 w-17 ml-2" />
-            </div>
+        <>
+            <Toaster position="top-center" reverseOrder={false} />
+            <div>
+                <div className="flex items-center">
+                    <hr className="border-t-2 w-15 ml-4" />
+                    <p className="ml-4 mr-2">Or continue with</p>
+                    <hr className="border-t-2 w-17 ml-2" />
+                </div>
 
-            <div className="flex justify-center items-center pt-2">
-                <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-                    <div >
-                        <GoogleLogin
-                            onSuccess={handleGoogleLogin}
-                            onError={() => console.log("Google Login Failed")}
-                        />
-                    </div>
-                </GoogleOAuthProvider>
-                {/* <img className="w-9.5 h-9.5" src="/DigiLocker_Logo.png" alt="DigiLocker Logo" /> */}
+                <div className="flex justify-center items-center pt-2">
+                    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+                        <div >
+                            <GoogleLogin
+                                onSuccess={handleGoogleLogin}
+                                onError={() => console.log("Google Login Failed")}
+                            />
+                        </div>
+                    </GoogleOAuthProvider>
+                    {/* <img className="w-9.5 h-9.5" src="/DigiLocker_Logo.png" alt="DigiLocker Logo" /> */}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
