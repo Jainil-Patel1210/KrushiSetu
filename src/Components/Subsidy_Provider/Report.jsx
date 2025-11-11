e89f82beeeca3d9666283861ee15de64ea37dc79
JeelPrajapati23 <202301238@daiict.ac.in>
2025-11-12 00:15:36 +0530

    Added Subsidy Provider Dashboard and Sidebar

---
+++ src/Components/Subsidy_Provider/Report.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../User_Profile/Header'
+import Settings from '../HomePage/Settings'
+const Report = () => {
+  return (
+    <>
+    <Header/>
+    <Settings/>
+    </>
+  )
+}
+
+export default Report
