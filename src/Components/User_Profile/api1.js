6e7f4df2ab61ef8df1f17ed00ba8dd9417a58105
desairudra <202301208@dau.ac.in>
2025-11-14 16:20:59 +0530

    api update

--- src/Components/User_Profile/api1.js
+++ src/Components/User_Profile/api1.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-BASE_URL = `${import.meta.env.VITE_BASE_URL}`
+const BASE_URL = `${import.meta.env.VITE_BASE_URL}`;
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
