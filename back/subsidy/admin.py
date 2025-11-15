43c2d4adac00a6a02e580f2f8fb536f1890b3c37
desairudra <desairudra4366@gmail.com>
2025-11-15 22:03:42 +0530

    some bugs fixed

--- back/subsidy/admin.py
+++ back/subsidy/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Document, Subsidy, SubsidyApplication
+from .models import Document,SubsidyApplication
 
 admin.site.register(Document)
-admin.site.register(Subsidy)
 admin.site.register(SubsidyApplication)
